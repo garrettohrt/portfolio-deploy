893b908d07bf4882584359af9daece482ace340a
gmun <garrettohrt@gmail.com>
2023-03-08 11:22:03 -0600

    add Resume link

--- src/screens/Contact/Contact.jsx
+++ src/screens/Contact/Contact.jsx
@@ -1,9 +1,13 @@
 import "./Contact.css"
+import Resume from "./garrett-resume-2023.pdf"
 function Contact() {
   return (
     <div id="contact" className="contact-container">
         <p>Garrett Ohrt</p>
         <p>Minneapolis, MN</p>
+        <a href ={Resume} target="_blank" rel="noopener noreferrer">
+          <p>Resume</p>
+        </a>
         <p>(563) 880-9503</p>
         <p>garrettohrt@gmail.com</p>
 
